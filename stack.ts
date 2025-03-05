e967f547a5bda6d4774cf639dae006cb98090294
ArmanBhatti <bhattiarman17@gmail.com>
2025-03-05 15:42:44 +0500

    dix db issue and remove unwanted variables

--- stack.ts
+++
@@ -1,6 +0,0 @@
-import "server-only";
-import { StackServerApp } from "@stackframe/stack";
-
-export const stackServerApp = new StackServerApp({
-  tokenStore: "nextjs-cookie", // storing auth tokens in cookies
-});
