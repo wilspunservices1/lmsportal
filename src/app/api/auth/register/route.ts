1cb43da4b2ebd3e77c2126fc329336501f8763d8
JayeshChak <thewabisabiwaylearnjc@gmail.com>
2025-03-07 20:39:50 +0530

    jc changes in register api

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 // import bcrypt from "bcrypt";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import { db } from "../../../../db/index";
 import { user } from "../../../../db/schemas/user";
 import { eq, sql } from "drizzle-orm";
@@ -10,105 +10,88 @@ import { BASE_URL } from "@/actions/constant";
 import { generateUniqueIdentifier } from "@/utils/generateUniqueIdentifier"; // Create this utility function
 
 function generateNameFromUsername(username: string): string {
-  const parts = username.split(" ");
-  if (parts.length >= 2) {
-    return `${parts[0]} ${parts[1]}`;
-  }
-  return username;
+	const parts = username.split(" ");
+	if (parts.length >= 2) {
+		return `${parts[0]} ${parts[1]}`;
+	}
+	return username;
 }
 
 // POST handler for user registration
 export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const userData = body;
+	try {
+		const body = await req.json();
+		const { email, password, username } = body;
 
-    // Check for required fields
-    if (
-      !userData?.email ||
-      !userData?.password ||
-      !userData?.username 
-    ) {
-      return NextResponse.json(
-        { message: "Missing required fields" },
-        { status: 400 }
-      );
-    }
+		// Check for required fields
+		if (!email || !password || !username) {
+			return NextResponse.json(
+				{ message: "Missing required fields" },
+				{ status: 400 }
+			);
+		}
 
     // Check for duplicate email
-    const duplicateEmail = await db
-      .select()
-      .from(user)
-      .where(eq(user.email, userData.email))
-      .then((res) => res);
+		const existingUser = await db
+			.select()
+			.from(user)
+			.where(eq(user.email, email))
+			.then((res) => res);
+		if (existingUser.length > 0) {
+			return NextResponse.json(
+				{
+					message:
+						"Email already registered. Please login or verify your email.",
+				},
+				{ status: 409 }
+			);
+		}
 
-    // Check for duplicate username
-    const duplicateUsername = await db
-      .select()
-      .from(user)
-      .where(eq(user.username, userData.username))
-      .then((res) => res);
-
-    if (duplicateEmail.length > 0 || duplicateUsername.length > 0) {
-      return NextResponse.json(
-        { message: "Duplicate Email or Username" },
-        { status: 409 }
-      );
-    }
-
-    // Hash the password
-    const hashPassword = await bcrypt.hash(userData.password, 10);
-
-    // Generate activation token
+    const hashPassword = await bcrypt.hash(password, 10);
     const activationToken = uuidv4();
+    const uniqueIdentifier = await generateUniqueIdentifier('user');
 
-    // Generate unique identifier
-    const uniqueIdentifier = await generateUniqueIdentifier('user'); // Use the appropriate role if needed
-
-    // Create the user
-    const newUser = await db
-      .insert(user)
-      .values({
-        uniqueIdentifier, // Assign the unique identifier
-        email: userData.email,
-        phone: userData.phone,
-        password: hashPassword, // Save the hashed password
-        username: userData.username,
-        name: generateNameFromUsername(userData.username).trim(),
-        image: userData.image,
-        roles: sql`'["user"]'::json`, // Assign default role as 'user'
-        isVerified: false, // Assuming the user is not verified at creation
-        activationToken, // Store the activation token
-      })
-      .returning()
-      .then((res) => res);
-
-    // Send activation email using the centralized sendEmail function
-    await sendEmail({
-      to: userData.email,
+	
+		// Send activation email using the centralized sendEmail function
+    const emailSent = await sendEmail({
+      to: email,
       subject: "Activate Your Account",
       text: "Please activate your account.",
       templateName: "activationEmailTemplate",
       templateData: {
-        name: userData.username,
+        name: username,
         activationLink: `${BASE_URL}/pass/activate?token=${activationToken}`,
       },
     });
-    
-    
 
-    return NextResponse.json(
-      {
-        message:
-          "User created. Please check your email to activate your account.",
-      },
-      { status: 201 }
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Error", error: error.message },
-      { status: 500 }
-    );
-  }
+    if (!emailSent) {
+      return NextResponse.json({ message: "Failed to send verification email. Try again later." }, { status: 500 });
+    }
+
+    // Insert user into the database only if email is successfully sent
+    await db.insert(user).values({
+      uniqueIdentifier,
+      email,
+      password: hashPassword,
+      username,
+      name: username.trim(),
+      roles: sql`'["user"]'::json`,
+      isVerified: false,
+      activationToken,
+    });
+
+		return NextResponse.json(
+			{
+				message:
+					"User created. Please check your email to activate your account.",
+			},
+			{ status: 201 }
+		);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{ message: "Error", error: error.message },
+			{ status: 500 }
+		);
+	}
 }
